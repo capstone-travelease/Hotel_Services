5ff1b8345c491fd4b17bba66ef5f2563d701b9e2
Tuan Phan Le <coderbillzay@gmail.com>
2023-12-25 09:15:32 +0700

    add city & country for output api

--- src/main/java/com/capstone/Hotel/Entities/HotelDetail.java
+++ src/main/java/com/capstone/Hotel/Entities/HotelDetail.java
@@ -9,6 +9,8 @@ public class HotelDetail {
     private Integer hotel_id;
     private String hotel_name;
     private String hotel_address;
+    private String hotel_city;
+    private String hotel_country;
     private String hotel_description;
     private Double star_rating;
     private String hotel_images;
@@ -17,19 +19,23 @@ public class HotelDetail {
     public HotelDetail() {
     }
 
-    public HotelDetail(Integer hotel_id, String hotel_name, String hotel_address, String hotel_description, Double star_rating, String hotel_images) {
+    public HotelDetail(Integer hotel_id, String hotel_name, String hotel_address, String hotel_city, String hotel_country, String hotel_description, Double star_rating, String hotel_images) {
         this.hotel_id = hotel_id;
         this.hotel_name = hotel_name;
         this.hotel_address = hotel_address;
+        this.hotel_city = hotel_city;
+        this.hotel_country= hotel_country;
         this.hotel_description = hotel_description;
         this.star_rating = star_rating;
         this.hotel_images = hotel_images;
     }
 
-    public HotelDetail(Integer hotel_id, String hotel_name, String hotel_address, String hotel_description, Double star_rating, String hotel_images, List<ResponseFacility> facilities) {
+    public HotelDetail(Integer hotel_id, String hotel_name, String hotel_address, String hotel_city, String hotel_country, String hotel_description, Double star_rating, String hotel_images, List<ResponseFacility> facilities) {
         this.hotel_id = hotel_id;
         this.hotel_name = hotel_name;
         this.hotel_address = hotel_address;
+        this.hotel_city = hotel_city;
+        this.hotel_country= hotel_country;
         this.hotel_description = hotel_description;
         this.star_rating = star_rating;
         this.hotel_images = hotel_images;
@@ -58,6 +64,22 @@ public class HotelDetail {
 
     public void setHotel_address(String hotel_address) {
         this.hotel_address = hotel_address;
+    }
+
+    public String getHotel_city() {
+        return hotel_city;
+    }
+
+    public void setHotel_city(String hotel_city) {
+        this.hotel_city = hotel_city;
+    }
+
+    public String getHotel_country() {
+        return hotel_country;
+    }
+
+    public void setHotel_country(String hotel_country) {
+        this.hotel_country = hotel_country;
     }
 
     public String getHotel_description() {
@@ -98,10 +120,12 @@ public class HotelDetail {
                 "hotel_id=" + hotel_id +
                 ", hotel_name='" + hotel_name + '\'' +
                 ", hotel_address='" + hotel_address + '\'' +
+                ", hotel_city='" + hotel_city + '\'' +
+                ", hotel_country='" + hotel_country + '\'' +
                 ", hotel_description='" + hotel_description + '\'' +
                 ", star_rating=" + star_rating +
                 ", hotel_images='" + hotel_images + '\'' +
-                ", facilities='" + facilities + '\'' +
+                ", facilities=" + facilities +
                 '}';
     }
 }
--- src/main/java/com/capstone/Hotel/Repositories/HotelRepository.java
+++ src/main/java/com/capstone/Hotel/Repositories/HotelRepository.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @Repository
 public interface HotelRepository extends JpaRepository<Hotels, Long> {
-    @Query(value = "SELECT new com.capstone.Hotel.Entities.HotelDetail(h.hotel_id, h.hotel_name, h.hotel_address, h.hotel_description, h.star_rating, atc.file_url) FROM Hotels h\n" +
+    @Query(value = "SELECT new com.capstone.Hotel.Entities.HotelDetail(h.hotel_id, h.hotel_name, h.hotel_address, h.hotel_city, h.hotel_country, h.hotel_description, h.star_rating, atc.file_url) FROM Hotels h\n" +
             "INNER JOIN HotelAttachment hatc ON hatc.hotel_id = h.hotel_id\n" +
             "INNER JOIN Attachment atc ON atc.attachment_id = hatc.attachment_id\n" +
             "WHERE h.hotel_id = ?1")
